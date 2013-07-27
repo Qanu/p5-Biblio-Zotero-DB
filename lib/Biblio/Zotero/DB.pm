e4debeb72bf64c7e77e7fe4b2550bc93bf7c0ed8
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-07-27 15:49:33 -0500

    Build results of 9aa8f86 (on master)

--- lib/Biblio/Zotero/DB.pm
+++ lib/Biblio/Zotero/DB.pm
@@ -7,7 +7,11 @@ package Biblio::Zotero::DB;
 use strict;
 use warnings;
 
+use v5.14;
 use Moo;
+use File::HomeDir;
+use Path::Class;
+use Path::Iterator::Rule;
 
 
 has schema => ( is => 'rw' );
@@ -22,8 +26,78 @@ has profile_name => ( is => 'rw' );
 
 
 
-sub find_profile_directories {
+# From <http://www.zotero.org/support/zotero_data>
+# Zotero for Firefox
+#
+# OS X                          /Users/<username>/Library/Application Support/Firefox/Profiles/<randomstring>/zotero
+# Windows 7/Vista               C:\Users\<User Name>\AppData\Roaming\Mozilla\Firefox\Profiles\<randomstring>\zotero
+# Windows XP/2000               C:\Documents and Settings\<username>\Application Data\Mozilla\Firefox\Profiles\<randomstring>\zotero
+# Linux (most distributions)    ~/.mozilla/firefox/Profiles/<randomstring>/zotero
 
+# Zotero Standalone
+#
+# OS X                          /Users/<username>/Library/Application Support/Zotero/Profiles/<randomstring>/zotero
+# Windows 7/Vista               C:\Users\<User Name>\AppData\Roaming\Zotero\Profiles\<randomstring>\zotero
+# Windows XP/2000               C:\Documents and Settings\<username>\Application Data\Zotero\Profiles\<randomstring>\zotero
+# Linux (most distributions)    ~/.zotero/Profiles/<randomstring>/zotero
+
+
+sub find_profile_directories {
+	my ($self) = @_;
+	for($^O) {
+		return $self->_find_profile_directories_linux when('linux');
+		return $self->_find_profile_directories_osx when('darwin');
+		return $self->_find_profile_directories_win when('MSWin32');
+		default { return [] };
+	}
+}
+
+sub _find_profile_directories_osx {
+	my ($self);
+	my $my_data = dir(File::HomeDir->my_data)->absolute;
+	# gives /Users/<username>/Library/Application Support
+	my $find = [
+		dir($my_data, 'Firefox'),
+		dir($my_data, 'Zotero') ];
+	return $self->_find_profile_directories_under(@$find);
+}
+
+sub _find_profile_directories_win {
+	my ($self);
+	my $my_data = dir(File::HomeDir->my_data)->absolute;
+	# gives C:\Users\<User Name>\AppData\Local
+	# or C:\Documents and Settings\<User Name>\Local Settings\Application Data
+	# depending on OS version
+	if( $my_data->components(-1) eq "Local" ) {
+		# for Windows 7 / Vista
+		# this returns the \Local directory
+		# we want the \Roaming directory that is on the same level
+		$my_data = $my_data->parent->subdir('Roaming');
+	}
+	my $find = [
+		dir( $my_data, 'Mozilla','Firefox'),
+		dir( $my_data, 'Zotero') ];
+	return $self->_find_profile_directories_under(@$find);
+}
+
+sub _find_profile_directories_linux {
+	my ($self);
+	my $find =  [
+		dir(File::HomeDir->my_home , '.mozilla/firefox'),
+		dir(File::HomeDir->my_home , '.zotero/zotero') ];
+	return $self->_find_profile_directories_under(@$find);
+
+}
+
+sub _find_profile_directories_under {
+	my ($self, $dirs) = @_;
+	# finds either
+	# dir('Profiles',<randomstring>,'zotero')
+	# or
+	# dir(<randomstring>, 'zotero')
+	return [ Path::Iterator::Rule->new
+		->min_depth(1)->max_depth(3)
+		->dir->name('zotero')->all( @$dirs ) ];
 }
 
 1;
