0087c42496548bae1a362dd6d5bba1786e278275
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-07-29 01:26:10 -0500

    Build results of ac58c9c (on master)

--- lib/Biblio/Zotero/DB/Role/CopyDB.pm
+++ lib/Biblio/Zotero/DB/Role/CopyDB.pm
@@ -62,7 +62,7 @@ version 0.001
 It is likely to that you might have Zotero open while using
 L<Biblio::Zotero::DB>. This causes problems as the database file will be locked
 as long as Zotero is running. To get around that, applying this role will
-create a copy whenever the C<schema> attribute of L<Biblio::Zotero::DB> is
+create a copy whenever the C<L<schema|Biblio::Zotero::DB/schema>> attribute of L<Biblio::Zotero::DB> is
 built.
 
 =head1 ATTRIBUTES
